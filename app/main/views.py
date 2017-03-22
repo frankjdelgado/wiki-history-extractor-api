e7388746a3615765e203bda57c4621f413054fea
Francisco Delgado <frankjdelgado@gmail.com>
2017-03-22 16:59:22 -0400

    deletec pyc files and added favicon

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, Response
+import os
+from flask import Flask, request, Response, send_from_directory
 from . import main
 
 @main.route('/', methods=['GET'])
 def index():
-    return "Hello, World!"
+    return "Hello World!"
+
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(main.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
