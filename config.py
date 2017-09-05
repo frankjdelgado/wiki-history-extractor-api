170291551444770c9092a11162f1bdbc60c591b5
Francisco Delgado <frankjdelgado@gmail.com>
2017-09-04 20:08:26 -0400

    change port typo to int from os environment variable

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ class ProductionConfig(Config):
 
 class DigitalOceanConfig(Config):
     MONGO_HOST = os.environ.get('MONGO_HOST')
-    MONGO_PORT = os.environ.get('MONGO_PORT')
+    MONGO_PORT = int(os.environ.get('MONGO_PORT'))
     MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
     MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
     SSL_DISABLE = True
