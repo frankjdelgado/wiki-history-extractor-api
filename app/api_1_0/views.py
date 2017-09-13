de99072cda816f7f13950bd33553a7f52252f41c
Francisco Delgado <frankjdelgado@gmail.com>
2017-09-12 21:13:10 -0400

    delete unnecesary methods

--- app/api_1_0/views.py
+++ app/api_1_0/views.py
@@ -4,22 +4,3 @@ from app.tasks.app_tasks import hello
 from vendors.db_connector import RevisionDB
 from config import config, Config
 from bson.json_util import dumps
-
-# Testing routes
-@api.route('/hello', methods=['GET'])
-def hello_test():
-	'''Testing function'''
-	task = hello.apply_async()
-	return jsonify({'Location': url_for('.task_status',task_id=task.id, name='hello')}), 202
-
-@api.route('/test', methods=['GET'])
-def test():
-	'''Testing function'''
-	db = RevisionDB(config={'host': config['default'].MONGO_HOST, 'port': config['default'].MONGO_PORT, 'username': config['default'].MONGO_USERNAME, 'password': config['default'].MONGO_PASSWORD})
-
-	# Number of revisions to process
-	value = dumps(db.db.revisions.find({'_id': 1}))
-
-
-	# Paginate and format chunks of data
-	return jsonify({'value': value}), 200
