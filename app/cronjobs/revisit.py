f5610e107dd25dfa0baee8dc40e98796bb5d4db7
KoreneroK <marvin.bernal@gmx.com>
2017-10-06 12:31:52 -0400

    Modified revisit probabilistic model. Now it will be used an Exponential distribuition to determine Expected value for time between revisions.
    Advances #20

--- app/cronjobs/revisit.py
+++ app/cronjobs/revisit.py
@@ -3,7 +3,7 @@ import time
 from math import factorial, exp
 from config import config, Config
 from vendors.db_connector import RevisionDB
-#from app.tasks.app_tasks import extract_article
+from app.tasks.app_tasks import extract_article
 
 # Based on wikimetrics ucv formula
 # t0: last time the article was updated
@@ -19,12 +19,27 @@ def calculate_v1(t0):
 		return False
 
 # TODO: get proper probability distribution to calculat if an article should be revisited
-def calculate_v2(k=1,mean=1):
-    result = poisson(k, mean)
-    if result > 0.5:
-        return True
-    else:
-        return False
+def calculate_v2(last_date,nth_date,amount_revisions):
+
+	#Lambda its calculated using the formula: Lambda= n / (t1 -t0)
+	seconds=(last_date - nth_date).total_seconds() + 1
+
+	lambda_exp= amount_revisions / (seconds*1.0)
+
+	#Expected value ( E(x) = 1 /Lambda) is calculated
+	expected_value= 1/(lambda_exp*1.0)
+	#print expected_value
+
+	#we get the time that has passed between the current time and the date of last revision
+	time_since_last_revision= 	(datetime.now()-last_date).total_seconds() + 1
+	#print time_since_last_revision
+
+	#if the time that has passed between the current time and the date of last revision is greater than the expected value
+	#the article shold be revisited
+	if time_since_last_revision > expected_value:
+		return True
+	else:
+		return False
 
 def poisson(k, mean):
 	# Formula: e**(-mean) * (mean**k / factorial(k))
@@ -100,17 +115,31 @@ def check_revisions(db):
 			{'$limit': per_page},
 		]
 		cursor = db.revisions().aggregate(pipeline=pipe)
-        last_rev= db.find_last_rev()
+		last_rev= db.find_last_rev()
 		for document in cursor: 
 		 	
 		 	# Set revision date to YYYY-MM-DD 00:00:00
-			date = document['date'].replace(hour=0, minute=0, second=0)
+			#date = document['date'].replace(hour=0, minute=0, second=0)
+			date = document['date']
 			
 			# Convert to integer
-			date_integer = int(time.mktime(date.timetuple()))
+			#date_integer = int(time.mktime(date.timetuple()))
+
+			#get total number of revisions for an article
+			total_revisions= db.revisions().find({'title':document['_id']}).count()
+
+			#get 10% of total_revisions or 20, whichever is greater
+			if total_revisions < 20:
+				amount_revisions= total_revisions
+			else:
+				amount_revisions=max(20,total_revisions*0.1)
+
+			#take the date of the nth revision, base of the amount_revisions, to calculate how much time has passed
+			nth_date=db.get_nth_revision_date(title=document['_id'],n=amount_revisions)
+			#nth_date_integer= int(time.mktime(nth_date.timetuple()))
 
 			# Verify if article should be revisited
-			should_revisit = calculate_v1(date_integer)
+			should_revisit = calculate_v1(date,nth_date,amount_revisions)
 			
 			if should_revisit:
 				# Revisit article. Extract revisions. Add to celery queue
@@ -120,5 +149,9 @@ db = RevisionDB(config={'host': config['default'].MONGO_HOST, 'port': config['de
 
 # Run algorithm
 check_revisions(db)
-calculate_v2(1.5,1)
+#date1=datetime(2015, 7, 14, 12, 30)
+#date2=datetime(2017, 7, 14, 12, 30)
+#calculate_v2(date2,date1,9)
  
+#Lambda= 350.88
+#E(x) = 0,002849977
