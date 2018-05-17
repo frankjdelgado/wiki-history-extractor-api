1ac2ca43127cc5e39a771f3e0e5f334ae5602a2d
Francisco Delgado <frankjdelgado@gmail.com>
2018-05-17 19:55:37 -0400

    assign current date to t1 instead of last date a revision was added to the article

--- app/cronjobs/revisit.py
+++ app/cronjobs/revisit.py
@@ -6,6 +6,7 @@ from vendors.db_connector import RevisionDB
 from app.tasks.app_tasks import extract_article
 import os
 
+
 env_name = os.getenv('FLASK_CONFIG') or 'default'
 
 
@@ -14,7 +15,7 @@ env_name = os.getenv('FLASK_CONFIG') or 'default'
 # t1: current time
 def calculate_v1(t0):
 	ti = int(time.mktime(datetime.now().timetuple()))
-	
+
 	result = 1 - (t0/ti)
 
 	if result > 0:
@@ -22,11 +23,12 @@ def calculate_v1(t0):
 	else:
 		return False
 
-# TODO: get proper probability distribution to calculat if an article should be revisited
 def calculate_v2(last_date,nth_date,amount_revisions):
+
 
 	#Lambda its calculated using the formula: Lambda= n / (t1 -t0)
 	seconds=(last_date - nth_date).total_seconds() + 1
+
 
 	lambda_exp= amount_revisions / (seconds*1.0)
 
@@ -38,15 +40,26 @@ def calculate_v2(last_date,nth_date,amount_revisions):
 	time_since_last_revision= 	(datetime.now()-last_date).total_seconds() + 1
 	#print time_since_last_revision
 
+	print ">>>> Start <<<<"
+	print "> E(x) = 1 / Lambda"
+	print "> E(x) = 1 / (n / (t1 -t0))"
+	print "> t1 =", last_date
+	print "> t0 =", nth_date
+	print "> n =", amount_revisions
+	print "> E(x) = ", expected_value
+	print "> Time Since Last Revision = ", time_since_last_revision
 	#if the time that has passed between the current time and the date of last revision is greater than the expected value
 	#the article shold be revisited
 	if time_since_last_revision > expected_value:
+		print "> Result: The article shold be revisited"
+		print ">>>> END <<<<"
 		#print 'True'
 		return True
 	else:
 		#print 'False'
+		print "> Result: The article shold NOT be revisited"
+		print ">>>> END <<<<"
 		return False
-
 def poisson(k, mean):
 	# Formula: e**(-mean) * (mean**k / factorial(k))
 
@@ -58,7 +71,7 @@ def poisson(k, mean):
 
 def binomial(n,x, p, q):
 	return 	p**x * q**(n-x) * factorial(n) / (factorial(x) * factorial(n-x))
-	
+
 
 # Yearly avg amoung of revisions per article
 def query_avg(db, pageid):
@@ -71,10 +84,10 @@ def query_avg(db, pageid):
 
 	cursor = db.revisions().aggregate(pipeline=pipe)
 	result = None
-	
-	for document in cursor: 
+
+	for document in cursor:
 		result = document
-	
+
 	if result != None:
 		result = result['avg_count']
 
@@ -90,10 +103,10 @@ def last_revision(pageid):
 
 	cursor = db.revisions().aggregate(pipeline=pipe)
 	result = None
-	
-	for document in cursor: 
+
+	for document in cursor:
 		result = document
-	
+
 	if result != None:
 		result = result['date']
 
@@ -123,12 +136,11 @@ def check_revisions(connection):
 			{'$limit': per_page},
 		]
 		cursor = connection.db.revisions.aggregate(pipeline=pipe)
-		for document in cursor: 
-		 	
-		 	# Set revision date to YYYY-MM-DD 00:00:00
-			#date = document['date'].replace(hour=0, minute=0, second=0)
-			date = document['date']
-			
+		for document in cursor:
+
+			# Get current date
+			date = datetime.now()
+
 			# Convert to integer
 			#date_integer = int(time.mktime(date.timetuple()))
 
@@ -147,7 +159,7 @@ def check_revisions(connection):
 
 			# Verify if article should be revisited
 			should_revisit = calculate_v2(date,nth_date,amount_revisions)
-			
+
 			if should_revisit:
 				# Revisit article. Extract revisions. Add to celery queue
 				extract_article.delay(document['_id'])
@@ -159,6 +171,6 @@ check_revisions(db)
 #date1=datetime(2015, 7, 14, 12, 30)
 #date2=datetime(2017, 7, 14, 12, 30)
 #calculate_v2(date2,date1,9)
- 
+
 #Lambda= 350.88
 #E(x) = 0,002849977
